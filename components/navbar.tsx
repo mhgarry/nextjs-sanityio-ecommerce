04a5dba34c14cfd5a69db4518129c627741fdb58
mhgarry <mhgarry92@gmail.com>
2024-07-17 18:17:19 -0400

    clean up ui

---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+
+export default function Navbar() {
+    return (
+        <nav className="w-full relative flex item-center justify-between max-w-2xl mx-auto px-4 py-5">
+            <Link href="/" className="bold text-3xl">
+                The<span className="text-primary ml-2">
+                Emporium
+                </span>
+            </Link>
+        </nav>
+    )
+}
